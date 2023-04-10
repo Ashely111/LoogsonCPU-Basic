592c84d164bc23f29ececda1b8e7dad7230d303d
qilin <1982043113@qq.com>
2023-04-10 23:15:30 +0800

    nothing newe

--- Id.v
+++ Id.v
@@ -7,3 +7,38 @@
 //
 //////////////////////////////////////////////////////////////
 
+`include "Defines.v"
+module Id (
+    input wire rst,
+    input wire[`InstAddrBus] pc_i,
+    input wire[`InstBus] inst_i,
+
+    // input regfile values
+    input wire[`RegBus] reg1_data_i,
+    input wire[`RegBus] reg2_data_i,
+    
+
+    //output regfile values
+    output reg reg1_read_o,
+    output reg reg2_read_o,
+    output reg[`RegAddrBus] reg1_addr_o,
+    output reg[`RegAddrBus] reg2_addr_o,
+
+    //transent to  id2ex
+    output reg[`RegBus] reg1_data_o,
+    output reg[`RegBus] reg2_data_o,
+    output reg[`RegAddrBus] waddr_o,
+    output reg reg_write_o
+
+);
+
+
+
+
+
+
+
+
+
+
+endmodule
--- If2Id.v
+++ If2Id.v
@@ -18,7 +18,15 @@ module If2Id (
     output reg[`InstBus] id_inst
 
 );
-    
+always @(posedge clk) begin
+    if (rst==`RstEnable) begin
+        id_pc <=`ZeroWord;
+        id_inst <=`ZeroWord;
+    end else begin
+        id_pc <=if_pc;
+        id_inst <=if_inst;
+    end
+end    
 endmodule
 
 
