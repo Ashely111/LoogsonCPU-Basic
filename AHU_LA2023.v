511157a857ec96b13be65fd3d670418d774783f5
qilin <1982043113@qq.com>
2023-04-13 07:21:26 +0800

    edit something

--- AHU_LA2023.v
+++ AHU_LA2023.v
@@ -6,3 +6,21 @@
 //
 //
 //////////////////////////////////////////////////////////////
+
+`include "Defines.v"
+module AHU_LA2023 (
+   input wire clk,
+   input wire rst,
+    input wire [`RegBus] rom_data_i,
+    output wire [`InstAddrBus] inst_addr_o   
+);
+//conect if2id and id
+wire [`InstAddrBus] if2id_pc_id ;
+wire [`InstBus] if2id_inst_id ;
+
+
+//conect id and id2ex
+
+wire [`] ;
+
+endmodule
